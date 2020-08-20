016aeedd0a028d0aba4408a216c63bc194adc93f
z-x-yang <57658428+z-x-yang@users.noreply.github.com>
2020-08-20 23:35:06 +0800

    Update ytb_eval_fast.sh
    Remove the edge setting.

--- ytb_eval_fast.sh
+++ ytb_eval_fast.sh
@@ -1,4 +1,4 @@
 config="configs.resnet101_cfbi"
 datasets="youtubevos"
 ckpt_path="./pretrain_models/resnet101_cfbi.pth"
-python tools/eval_net.py --config ${config} --dataset ${datasets} --ckpt_path ${ckpt_path}  --float16 --global_atrous_rate 2  --max_long_edge 800
+python tools/eval_net.py --config ${config} --dataset ${datasets} --ckpt_path ${ckpt_path}  --float16 --global_atrous_rate 2
